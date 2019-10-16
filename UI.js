1aaba280b6e2813f1f14711fbdac3cadccfa66ac
Martin Paju <fezzzle@gmail.com>
2019-10-16 18:13:25 +0300

    16.10.19 night update

--- UI.js
+++ UI.js
@@ -53,17 +53,70 @@ class UI {
     this.addRollDiceButton();
     this.displayDiceResult("Start");
   }
-  addLadder(pos1, pos2) {
-    const createDiv = document.createElement("div");
-    const elementToAttchtoOne = document.getElementById(pos1);
-    const elementToAttchtoTwo = document.getElementById(pos2);
-    let elementToAttchToOneObj = elementToAttchtoOne.getBoundingClientRect();
-    console.log("TCL: UI -> addLadder -> First element", elementToAttchToOneObj)
-    console.log("TCL: UI -> addLadder -> First element Y COORDS", elementToAttchToOneObj.y)
-    let elementToAttchToTwoObj = elementToAttchtoTwo.getBoundingClientRect();
-    console.log("TCL: UI -> addLadder -> Second element", elementToAttchToTwoObj)
-    console.log("TCL: UI -> addLadder -> Second element Y COORDS", elementToAttchToTwoObj.y)
-  }
+  // addLadder(pos1, pos2) {
+  //   const elementToAttchtoOne = document.getElementById(pos1);
+  //   const elementToAttchtoTwo = document.getElementById(pos2);
+
+  //   const el1Coords = elementToAttchtoOne.getBoundingClientRect();
+  //   const el2Coords = elementToAttchtoTwo.getBoundingClientRect();
+  //   console.log("TCL: UI -> addLadder -> el1Coords", el1Coords)
+  //   console.log("TCL: UI -> addLadder -> el2Coords", el2Coords)
+
+    // console.log(ladder.style);
+    // ladder.style.width = `20px`;
+    // ladder.style.height = `300px`;
+    // ladder.style.top = `${el1Coords.top}px`
+    // ladder.style.left = `${el1Coords.left}px`
+
+    // ladder.classList.add("ladder");
+    // document.body.append(ladder);
+  // }
 }
 
+function createLineElement(x, y, length, angle) {
+  var line = document.createElement("div");
+  var styles = 'border: 1px solid black; '
+             + 'width: ' + length + 'px; '
+             + 'height: 0px; '
+             + '-moz-transform: rotate(' + angle + 'rad); '
+             + '-webkit-transform: rotate(' + angle + 'rad); '
+             + '-o-transform: rotate(' + angle + 'rad); '  
+             + '-ms-transform: rotate(' + angle + 'rad); '  
+             + 'position: absolute; '
+             + 'top: ' + y + 'px; '
+             + 'left: ' + x + 'px; ';
+  line.setAttribute('style', styles);  
+  return line;
+}
+
+function createLine(x1, y1, x2, y2) {
+  var a = x1 - x2,
+      b = y1 - y2,
+      c = Math.sqrt(a * a + b * b);
+
+  var sx = (x1 + x2) / 2,
+      sy = (y1 + y2) / 2;
+
+  var x = sx - c / 2,
+      y = sy;
+
+  var alpha = Math.PI - Math.atan2(-b, a);
+
+  return createLineElement(x, y, c, alpha);
+
+}
+
+
+document.body.addEventListener("mouseup", getCoords);
+
+// function getCoords(e) {
+//   console.log(e);
+//   const elementToAttchtoOne = document.getElementById(14).getBoundingClientRect();
+//   console.log(elementToAttchtoOne);
+//   const elementToAttchtoTwo = document.getElementById(37).getBoundingClientRect();
+//   console.log(elementToAttchtoTwo);
+//   let savefirstCoord = (e.screenX - elementToAttchtoOne.x);
+//   let firstCoord = e.screenX - savefirstCoord;
+//   document.body.appendChild(createLineElement(firstCoord, elementToAttchtoTwo.x, 400, 5));
+// }
 
